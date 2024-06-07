6ac79fb5de2c4c7b20026088885eb1a570e23251
Zetni <azakharov@workmovr.com>
2024-06-07 22:05:38 +0400

    fix bugs + new fields

--- app/lib/caspio/client.rb
+++ app/lib/caspio/client.rb
@@ -74,6 +74,7 @@ module Caspio
         'Record_PK' => data.Record_PK,
         'Parent_Farm' => data.Parent_Farm_FK,
         'Field_ID' => data.Field_ID,
+        'Field_Acres' => data.Field_Acres,
         'Active_YN' => data.Active_YN,
       }
     end
@@ -100,10 +101,15 @@ module Caspio
         'Start_Minutes_KW' => data['Start_Time_Minutes'],
         'End_Hours_KW' => data['End_Time_Hours'],
         'End_Minutes_KW' => data['End_Time_Minutes'],
-        'Total_Time_Hours' => (data['Total_Hours'].present? && !data['Total_Hours'].empty?) ? data['Total_Hours'] : nil,
-        'Tons_Baled' => (data['Tons_Baled'].present? && !data['Tons_Baled'].empty?) ? data['Tons_Baled'] : nil,
-        'Bales_Baled' => (data['Bales_Baled'].present? && !data['Bales_Baled'].empty?) ? data['Bales_Baled'] : nil,
-        'Average_Moisture_Percent' => (data['Moisture'].present? && !data['Moisture'].empty?) ? data['Moisture'] : nil,
+        'Total_Time_Hours' => (data['Total_Hours'].present? && !data['Total_Hours'].empty? && data['Total_Hours'] != 'NaN') ? data['Total_Hours'] : nil,
+        'Tons_Baled' => (data['Tons_Baled'].present? && !data['Tons_Baled'].empty? && data['Tons_Baled'] != 'NaN')  ? data['Tons_Baled'] : nil,
+        'Bales_Baled' => (data['Bales_Baled'].present? && !data['Bales_Baled'].empty? && data['Bales_Baled'] != 'NaN')  ? data['Bales_Baled'] : nil,
+        'Average_Moisture_Percent' => (data['Moisture'].present? && !data['Moisture'].empty? && data['Moisture'] != 'NaN')  ? data['Moisture'] : nil,
+        'Acres' => (data['Acres'].present? && !data['Acres'].empty? && data['Acres'] != 'NaN')  ? data['Acres'] : nil,
+        'Tons_per_Acre' => (data['Tons_Acre'].present? && !data['Tons_Acre'].empty? && data['Tons_Acre'] != 'NaN') ? data['Tons_Acre'] : nil,
+        'Bales_per_Acre' => (data['Bales_Acre'].present? && !data['Bales_Acre'].empty? && data['Bales_Acre'] != 'NaN') ? data['Bales_Acre'] : nil,
+        'Tons_per_Hour' => (data['Tons_Hour'].present? && !data['Tons_Hour'].empty? && data['Tons_Hour'] != 'NaN')  ? data['Tons_Hour'] : nil,
+        'Bales_per_Hour' => (data['Bales_Hour'].present? && !data['Bales_Hour'].empty? && data['Bales_Hour'] != 'NaN')  ? data['Bales_Hour'] : nil,
         'Active_YN' => true,
       }
     end
@@ -127,10 +133,15 @@ module Caspio
         'Start_Minutes_KW' => data['Start_Time_Minutes'],
         'End_Hours_KW' => data['End_Time_Hours'],
         'End_Minutes_KW' => data['End_Time_Minutes'],
-        'Total_Time_Hours' => (data['Total_Hours'].present? && !data['Total_Hours'].empty?) ? data['Total_Hours'] : nil,
-        'Tons_Baled' => (data['Tons_Baled'].present? && !data['Tons_Baled'].empty?) ? data['Tons_Baled'] : nil,
-        'Bales_Baled' => (data['Bales_Baled'].present? && !data['Bales_Baled'].empty?) ? data['Bales_Baled'] : nil,
-        'Average_Moisture_Percent' => (data['Moisture'].present? && !data['Moisture'].empty?) ? data['Moisture'] : nil,
+        'Total_Time_Hours' => (data['Total_Hours'].present? && !data['Total_Hours'].empty? && data['Total_Hours'] != 'NaN') ? data['Total_Hours'] : nil,
+        'Tons_Baled' => (data['Tons_Baled'].present? && !data['Tons_Baled'].empty? && data['Tons_Baled'] != 'NaN')  ? data['Tons_Baled'] : nil,
+        'Bales_Baled' => (data['Bales_Baled'].present? && !data['Bales_Baled'].empty? && data['Bales_Baled'] != 'NaN')  ? data['Bales_Baled'] : nil,
+        'Average_Moisture_Percent' => (data['Moisture'].present? && !data['Moisture'].empty? && data['Moisture'] != 'NaN')  ? data['Moisture'] : nil,
+        'Acres' => (data['Acres'].present? && !data['Acres'].empty? && data['Acres'] != 'NaN')  ? data['Acres'] : nil,
+        'Tons_per_Acre' => (data['Tons_Acre'].present? && !data['Tons_Acre'].empty? && data['Tons_Acre'] != 'NaN') ? data['Tons_Acre'] : nil,
+        'Bales_per_Acre' => (data['Bales_Acre'].present? && !data['Bales_Acre'].empty? && data['Bales_Acre'] != 'NaN') ? data['Bales_Acre'] : nil,
+        'Tons_per_Hour' => (data['Tons_Hour'].present? && !data['Tons_Hour'].empty? && data['Tons_Hour'] != 'NaN')  ? data['Tons_Hour'] : nil,
+        'Bales_per_Hour' => (data['Bales_Hour'].present? && !data['Bales_Hour'].empty? && data['Bales_Hour'] != 'NaN')  ? data['Bales_Hour'] : nil,
         'Active_YN' => true,
       }
     end
